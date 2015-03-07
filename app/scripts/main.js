4f480cb82948bd42f927dc6d34fc9a6d868b2e44
Jessica-Caraballo <jessicasinga@gmail.com>
2015-03-07 17:23:35 +0100

    Redes sociales incluidas. Contenido. Retoques finales.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -38,4 +38,26 @@ $( document ).ready(function() {
 		}
 	window.onload =init;
 
+	/*Galeria*/
+	$(window).load(function() {
+	  
+	  $('#carousel').flexslider({
+	    animation: "slide",
+	    controlNav: false,
+	    animationLoop: false,
+	    slideshow: false,
+	    itemWidth: 210,
+	    itemMargin: 5,
+	    asNavFor: '#slider'
+	  });
+ 
+	  $('#slider').flexslider({
+	    animation: "slide",
+	    controlNav: false,
+	    animationLoop: false,
+	    slideshow: false,
+	    sync: "#carousel"
+	  });
+	});
+
 });
